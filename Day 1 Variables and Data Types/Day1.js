b0f1aee0ed3ddc6a956bdb3d2f92ec48644dc9b2
iamtayyab007 <tayyabm336@gmail.com>
2024-07-14 11:31:30 +0500

    add folders

---
+++ Day 1 Variables and Data Types/Day1.js
@@ -0,0 +1,75 @@
+// variable declaration
+
+var oddNumber = 3;
+console.log(oddNumber);
+let name = "Hussain";
+console.log(name);
+
+// constant declaration
+
+const value = false;
+console.log(value);
+
+// Data Types
+
+let number = 1234;
+let string = "name";
+let boolean = true;
+let object = {};
+
+let array = [];
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof object);
+console.log(typeof array);
+
+// Reassigning Variables
+
+let car = "Audi";
+
+car = "ferrari";
+
+console.log(car);
+
+// Understanding const
+
+const fruit = "guava";
+//fruit = "apple";
+
+console.log(fruit);
+
+// Feature Request
+
+// Q1
+
+let gun = "Ak47";
+console.log(gun, typeof gun);
+
+let random = 1234;
+console.log(random, typeof random);
+
+let village = {
+  name: "watni",
+  location: "Earth",
+};
+console.log(village, typeof village);
+
+let game = ["GTA", "doom", "IGI"];
+
+console.log(game, typeof game);
+
+let isGameOn = false;
+console.log(isGameOn, typeof isGameOn);
+
+// Q2
+
+let winner = true;
+winner = false;
+
+console.log(winner);
+
+// const loser = true;
+// loser = false;
+// console.log(loser);
---
+++ Day 2 Operators/Day2.js
@@ -0,0 +1,103 @@
+// Activity 1 Arithmetic Operations
+
+// Task 1
+
+let a = 2;
+let b = 3;
+let result1 = a + b;
+console.log(result1);
+
+// Task 2
+let c = 2;
+let d = 3;
+let result2 = c - d;
+console.log(result2);
+
+// Task 3
+let e = 2;
+let f = 3;
+let result3 = e * f;
+console.log(result2);
+
+// Task 4
+let g = 2;
+let h = 3;
+let result4 = g / h;
+console.log(result4);
+
+// Task 5
+
+let i = 2;
+let j = 3;
+let result5 = i % j;
+console.log(result5);
+
+// Activity 2 Assignment Operators
+// Task 6
+
+let addNumbers = 3;
+
+addNumbers += 4;
+console.log(addNumbers);
+
+// Task 7
+
+let subNumbers = 3;
+
+subNumbers -= 4;
+console.log(subNumbers);
+
+// Activity 3 Comparison Operators
+
+// Task 8
+
+let number1 = 22;
+let number2 = 77;
+
+compareResult1 = number1 > number2;
+compareResult2 = number1 < number2;
+compareResult3 = number1 >= number2;
+compareResult4 = number1 <= number2;
+compareResult5 = number1 == number2;
+compareResult6 = number1 === number2;
+console.log(
+  compareResult1,
+  compareResult2,
+  compareResult3,
+  compareResult4,
+  compareResult5,
+  compareResult6
+);
+
+// Activity 4 Logical Operators
+
+// Task 11
+
+let number3 = 45;
+let number4 = 55;
+
+logicOperands1 = number3 === 45 && number4 === 55;
+
+console.log(logicOperands1);
+
+// Task 12
+
+let number5 = 11;
+let number6 = 9;
+logicOperands2 = number5 === 11 || number6 === 10;
+console.log(logicOperands2);
+
+// task 13
+let number7 = 12;
+logicOperands3 = number7 !== 11;
+console.log(logicOperands3);
+
+// Activity 5 Ternary Operator
+
+// Task 14
+
+let checkNumber = -10;
+
+let checkResult =
+  checkNumber >= 0 ? "The number is positive" : "The number is negative";
+console.log(checkResult);

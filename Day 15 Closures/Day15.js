497dc40ec58a757d4599bd8e38ba7f82b2f452a7
iamtayyab007 <tayyabm336@gmail.com>
2024-08-05 16:15:18 +0500

    Day 15 JS Challenge in progress

--- Day 15 Closures/Day15.js
+++ Day 15 Closures/Day15.js
@@ -1,2 +1,28 @@
 // Activity 1
 // Task 1
+{
+  /*
+function greeting(greetings) {
+  return function (name) {
+    return `${greetings}, ${name}`;
+  };
+}
+
+const greetHi = greeting("Welcome");
+
+console.log(greetHi("janagu"));
+*/
+}
+
+function createCounter() {
+  let count = 0;
+  return function () {
+    count += 1;
+    return count;
+  };
+}
+
+const trackCounter = createCounter();
+console.log(trackCounter());
+console.log(trackCounter());
+console.log(trackCounter());
